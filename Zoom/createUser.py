ff9db11a32d9108b9ec604fbb63ccfa5e94cd638
Vishruth N <mail2vishruth@gmail.com>
2023-11-18 23:01:23 +0530

    seperate Zoom

--- Zoom/createUser.py
+++ Zoom/createUser.py
@@ -16,17 +16,18 @@ def generate_oauth_token():
     }
     response = requests.post(url, headers=headers, data=payload)
     token = response.json().get('access_token', None)
+    scopes = response.json().get('scope', None)
     if not token:
         raise Exception("Failed to retrieve access token")
-    return token
+    return token, scopes
 
 # Generate the OAuth token
-oauth_token = generate_oauth_token()
+oauth_token, scopes = generate_oauth_token()
 
 # Print the OAuth token
 print(oauth_token)
-
-# Set the authorization header with the OAuth token
+print("scopes: ", scopes)
+# Set the authorization header with the OAuth tokenD
 headers = {
     'Authorization': f"Bearer {oauth_token}",
     'Content-Type': "application/json"
@@ -34,13 +35,20 @@ headers = {
 
 # Payload for creating a new user
 payload = {
-    'action': 'create',
-    "user_info": {
-        "email": "test@gmail.com",
-        "type": 1,
-        "first_name": "Justin",
-        "last_name": "Test"
-    }
+  "action": "create",
+  "user_info": {
+    "email": "jchill@example.com",
+    "first_name": "Jill",
+    "last_name": "Chill",
+    "display_name": "Jill Chill",
+    "password": "if42!LfH@",
+    "type": 1,
+    "feature": {
+      "zoom_phone": True,
+      "zoom_one_type": 16
+    },
+    "plan_united_type": "1"
+  }
 }
 
 # Make the API request to create a new user
---
+++ Zoom/zhelper.py
@@ -0,0 +1,3 @@
+from zoom_api_helper import ZoomAPI
+zoom = ZoomAPI('dsO5edfeQneSJl0czptGMw', '1Q7p96uSdEJvHLFVbxGlhOTCPnCACHX4',account_id='zr3zB0K2RDy6jC4GLElf7A')
+print(zoom.list_users())
